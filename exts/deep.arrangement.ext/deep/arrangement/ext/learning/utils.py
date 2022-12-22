0d7ad256d9c708ccc8cacdc6c95544924f9046e9
yizhouzhao <yizhou_zhao@berkeley.edu>
2022-12-22 11:37:16 -0800

    sac debug

--- exts/deep.arrangement.ext/deep/arrangement/ext/learning/utils.py
+++ exts/deep.arrangement.ext/deep/arrangement/ext/learning/utils.py
@@ -2,6 +2,7 @@
 import os
 import numpy as np
 import torch
+from PIL import Image
 
 def extract_image_feature_and_save(image_array, feature_extractor, save_path, device = torch.device("cuda")):
     """
@@ -16,6 +17,14 @@ def extract_image_feature_and_save(image_array, feature_extractor, save_path, de
     x_feature = feature_extractor(x).squeeze()
     torch.save(x_feature.cpu().data, save_path)
 
+def extract_image_clip_feature_and_save(image_file, model, processor, save_path, device = torch.device("cuda")):
+    """
+    Extract clip feature and save to path
+    """
+    image = Image.open(image_file).convert('RGB')
+    inputs = processor(images=image, return_tensors="pt").to(device)    
+    x_feature = model.get_image_features(**inputs).squeeze().cpu().data
+    torch.save(x_feature.cpu().data, save_path)
 
 
 def soft_update_from_to(source, target, tau):
