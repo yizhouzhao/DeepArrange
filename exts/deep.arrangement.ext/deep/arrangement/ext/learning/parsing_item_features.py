56537dc15f5d937de8a31c98378144c0012be906
yizhouzhao <yizhou_zhao@berkeley.edu>
2022-12-21 20:06:48 -0800

    update feature to clip

--- exts/deep.arrangement.ext/deep/arrangement/ext/learning/parsing_item_features.py
+++ exts/deep.arrangement.ext/deep/arrangement/ext/learning/parsing_item_features.py
@@ -2,6 +2,9 @@ import os
 import torch
 from torchvision.io import ImageReadMode, read_image
 
+from PIL import Image
+from transformers import CLIPProcessor, CLIPModel
+
 EXTENSION_FOLDER_PATH = "/home/yizhou/Research/DeepArrange"
 
 ASSET_PATH = EXTENSION_FOLDER_PATH + "/Asset"
@@ -14,9 +17,10 @@ class ObjectFeatureParser():
     """
     Use resetnet to parser image features and save them into a folder
     """
-    def __init__(self, object_type) -> None:
+    def __init__(self, object_type, output_layer = None, model_type = "clip") -> None:
 
         # load asset
+        self.model_type = model_type
         self.object_type = object_type
         self.asset_path = ASSET_PATH # parsing local images only
         object_folder = [obj for obj in os.listdir(os.path.join(self.asset_path, "I", object_type))]
@@ -25,7 +29,11 @@ class ObjectFeatureParser():
 
         # load model
         self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        self.model = ResNetFeatureExtractor(output_layer='avgpool').to(self.device)
+        if self.model_type == "resnet":
+            self.model = ResNetFeatureExtractor(output_layer=output_layer).to(self.device) # avgpool
+        else:
+            self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(self.device)
+            self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
         # make saving folder
         self.save_path = os.path.join(FEATURE_PATH, object_type)
@@ -41,9 +49,17 @@ class ObjectFeatureParser():
         print("Extracting feature for object: ", self.object_list[idx])
         image_file = os.path.join(self.image_folder, obj + ".png")
 
-        x = read_image(image_file, mode=ImageReadMode.RGB)
-        x = x.unsqueeze(0).float().to(self.device)
-        x_feature = self.model(x).squeeze()
+        if self.model_type == "resnet":
+            x = read_image(image_file, mode=ImageReadMode.RGB)
+            x = x.unsqueeze(0).float().to(self.device)
+            x_feature = self.model(x).squeeze()
+            x_feature = x_feature.cpu().data
+        else:
+            # clip
+            image = Image.open(image_file).convert('RGB')
+            inputs = self.processor(images=image, return_tensors="pt").to(self.device)
+            x_feature = self.model.get_image_features(**inputs).cpu().data
+            
         torch.save(x_feature, os.path.join(self.save_path, obj_name + ".pt"))
 
     def parse_all_features(self):
